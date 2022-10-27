a2da01eea56fa066b8e4f95820520d67244045a3
niharika1607 <Niha@1607>
2022-10-27 09:46:08 +0200

    Created Hooks

--- src/main/java/com/payconiq/stepdefinition/Hooks.java
+++ src/main/java/com/payconiq/stepdefinition/Hooks.java
@@ -1,19 +1,18 @@
 package com.payconiq.stepdefinition;
 
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
-
 public class Hooks {
 
-	private static final Logger LOG = LogManager.getLogger(Hooks.class);
-	
-	@Before
-	public void testStart(Scenario scenario) {
-		LOG.info("*****************************************************************************************");
-		LOG.info("	Scenario: "+scenario.getName());
-		LOG.info("*****************************************************************************************");
-	}
+    private static final Logger LOG = LogManager.getLogger(Hooks.class);
+
+    @Before
+    public void testStart(Scenario scenario) {
+        LOG.info("*****************************************************************************************");
+        LOG.info("	Scenario: " + scenario.getName());
+        LOG.info("*****************************************************************************************");
+    }
 }
